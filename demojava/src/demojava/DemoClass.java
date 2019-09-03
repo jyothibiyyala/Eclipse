b90c168b7ae6a6f31c4d8a1c2f6008215448b07e
Sury <Sury@DESKTOP-CLTPBIE>
2019-09-03 11:28:13 +0530

    This is my first commit

---
+++ demojava/src/demojava/DemoClass.java
@@ -0,0 +1,15 @@
+package demojava;
+
+public class DemoClass {
+ 
+	public static void main(String[] args) {
+		int a=10;
+		 int b=20;
+		System.out.println("a and b values are not printed");
+		System.out.println(b);
+		System.out.println(a);
+		
+
+	}
+
+}
---
+++ demojava/src/module-info.java
@@ -0,0 +1,2 @@
+module demojavaproject {
+}
